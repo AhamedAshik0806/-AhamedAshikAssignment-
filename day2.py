7f486dac7fdef47cb2ab0a77b89300c7c77d807b
AhamedAshik0806 <84092202+AhamedAshik0806@users.noreply.github.com>
2021-05-12 09:26:08 -0700

    Day 2 Assignment Submitted

---
+++ day2.py
@@ -0,0 +1,9 @@
+list_brand1 = ["Google","Facebook","Instagram","Spotify"]
+list_brand2 = ["Quora","Netflix","Dropbox"]
+list_brand1.sort()
+list_brand2.sort()
+print("list 1 of brands in asscending order",list_brand1)
+print("list 2 of brands in asscending order",list_brand2)
+list_brand = list_brand1 + list_brand2
+list_brand.sort()
+print(list_brand) 
